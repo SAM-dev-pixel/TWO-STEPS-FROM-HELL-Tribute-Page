3df7ab18fa66bd08676d98ce1f0d3c117b0b2d91
SAM <75918417+SAM-dev-pixel@users.noreply.github.com>
2021-06-27 08:38:43 +0700

    Adding source code

---
+++ script.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '40px',
+  duration: 1000,
+  reset: true
+});
+sr.reveal('#tribute-link', {interval: 300});
+sr.reveal('.follow-icon', {origin: 'left', interval: 250});
